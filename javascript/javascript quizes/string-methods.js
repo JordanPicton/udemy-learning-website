99e2732889add6c74d00ad84d9e0b3a5f54fbaac
Jordan Picton <jordan_picton@hotmail.com>
2023-10-03 12:35:38 +0100

    Learning about slice

---
+++ javascript/javascript quizes/string-methods.js
@@ -0,0 +1,8 @@
+// DON'T TOUCH THIS LINE!
+const message = "    TASTE THE RAINBOW!  "; //DON'T CHANGE THIS LINE PLEASE!
+
+// YOUR CODE BELOW THIS LINE:
+// Without altering the original message above, create a new variable called "wshiper", removing all the extra white space removed
+//along with being lower cased.
+
+let whisper = message.trim().toLowerCase();
