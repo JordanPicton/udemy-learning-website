073f67f7f016c0e54257420a317f903f92278b29
Jordan Picton <jordan_picton@hotmail.com>
2023-10-28 20:36:17 +0100

    Completed Internal/External JS - Section 16 167

--- javascript/decision-making/internal-external.js
+++ javascript/decision-making/internal-external.js
@@ -1,1 +1,2 @@
+// Having the below code, just prints out the String between the quotation marks into the broswers console with the developer tools.
 console.log("Hello, this is the first JavaScript file I've made!");
