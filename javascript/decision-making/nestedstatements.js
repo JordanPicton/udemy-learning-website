0cf9818113406d7705d30a2f02f2291b9502bddb
Jordan Picton <jordan_picton@hotmail.com>
2023-11-08 23:20:10 +0000

    Start on Nested Conditions

---
+++ javascript/decision-making/nestedstatements.js
@@ -0,0 +1,21 @@
+// The example I'm about to paste below is already used and talked about within the conditionals.js file so I'll remove the comments from this one, leave the code but comment the code out.
+// The reason to why I've included this to begin with is to just show an example of how nested conditions can work.
+
+// ---------Example Start---------
+// let Num1 = prompt("Enter a number between 0 and 9.");
+// let Num2 = prompt("Enter a number between 0 and 9.");
+// let Num3 = prompt("Enter a number between 0 and 9,");
+
+// if (Num1 > Num2) {
+//   if (Num1 > Num3) {
+//     console.log(`Num1 (${Num1}) is bigger than Num3`);
+//   } else {
+//   }
+// } else {
+//   if (Num2 > Num3) {
+//     console.log(`Num2 (${Num2}) is bigger than Num3.`);
+//   } else {
+//     console.log(`Num3 (${Num3}) is bigger than Num2`);
+//   }
+// }
+// ---------Example End---------
