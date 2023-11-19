330eabe62e39176d4ff1899c8a4f1c50650000bb
Jordan Picton <jordan_picton@hotmail.com>
2023-11-19 00:12:47 +0000

    Completed Section 16 Chapter 174

--- javascript/decision-making/logical-and.js
+++ javascript/decision-making/logical-and.js
@@ -1,6 +1,6 @@
 const password = prompt("Enter a password");
 
-if (password.length >= 6 && password.indexOf(" ") === -1) {
+if  ((password.length >= 6 && password.indexOf(' ') === -1) || password.indexOf('i') > 5) {
   console.log("Nice password!");
 } else {
   console.log("The password CANNOT contain any space characters or be under 6 characters in length!");
