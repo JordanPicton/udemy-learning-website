2832c290c3c3e6d19c1888b375b5ae26b4028be8
Jordan Picton <jordan_picton@hotmail.com>
2023-11-20 21:38:09 +0000

    Completed Section 16 Chapter 174 Logic OR

--- javascript/decision-making/logical-or.js
+++ javascript/decision-making/logical-or.js
@@ -2,12 +2,12 @@
 const dog = "Charlie";
 const firstName = "Jordan";
 
-let input = prompt("What is the value of Dog OR Firstname?")
+let input = prompt("What is the value of Dog OR Firstname?");
 
-if (input === "Charlie" || input ==="Jordan") {
-    console.log("True");
+if (input === "Charlie" || input === "Jordan") {
+  console.log("True");
 } else {
-    console.log("False");
+  console.log("False");
 }
 
 // Using the commented out example below I'll be redoing it with including the OR operator.
@@ -16,28 +16,28 @@ if (input === "Charlie" || input ==="Jordan") {
 // If not less than 3
 // if (age <= 3) {
 //   console.log("You go in for free!");
-  // Then are you less than 12? (Only tested if the above condition is false.)
+// Then are you less than 12? (Only tested if the above condition is false.)
 // } else if (age <= 12) {
 //   console.log("Price is £3 to enter.");
-  // Then are you less than 17? (Only tested if the above condition is false.)
+// Then are you less than 17? (Only tested if the above condition is false.)
 // } else if (age <= 17) {
 //   console.log("Price is £5 to enter.");
-  // Then are you less than 30? (Only tested if the above condition is false.)
+// Then are you less than 30? (Only tested if the above condition is false.)
 // } else if (age <= 30) {
 //   console.log("Price is £10 to enter.");
-  // Then are you less than 60? (Only tested if the above condition is false.)
+// Then are you less than 60? (Only tested if the above condition is false.)
 // } else if (age <= 60) {
 //   console.log("Price is £15 to enter.");
-  // Then are you less than 80? (Only tested if the above condition is false.)
+// Then are you less than 80? (Only tested if the above condition is false.)
 // } else if (age <= 80) {
 //   console.log("Price is £10 to enter.");
-  // Then are you less than 100? (Only tested if the above condition is false.)
+// Then are you less than 100? (Only tested if the above condition is false.)
 // } else if (age <= 100) {
 //   console.log("Price is £3 to enter.");
-  // Then are you less than 120? (Only tested if the above condition is false.)
+// Then are you less than 120? (Only tested if the above condition is false.)
 // } else if (age <= 120) {
 //   console.log("You go in for free!");
-  // Everything else goes here. (Only tested if the above condition is false.)
+// Everything else goes here. (Only tested if the above condition is false.)
 // } else {
 //   alert("Make sure you use full numbers between 1-120 for your age!");
 // }
@@ -45,12 +45,14 @@ if (input === "Charlie" || input ==="Jordan") {
 let age = prompt("Insert age.");
 
 // So instead of having another expression later on for being over a certain age, I can just do this within the same expression.
-// This cleans up the code somewhat due to it not needing another clause to repeat something that's already being done just with a different value.
-// It's better to just add that extra condition.
-if (age <= 3 || age >= 65) {
-    console.log("You go in for free!");
-} else if (age <= 12) {
-    console.log("Price is £3 to enter.")
+// This cleans up the code somewhat due to it not needing another clause to repeat something that's already being done
+// just with a different value.
+// It's better to just add that extra condition. So the age 0 AND age 3 is one expression. With the OR age 65 being another.
+if ((age >= 0 && age <= 3) || age >= 65) {
+  console.log("You go in for free!");
+  // I added the age >= 0 && , right after that sentence.
+} else if (age >= 0 && age <= 12) {
+  console.log("Price is £3 to enter.");
 } else {
-    alert("Make sure you use full/whole numbers between 1-120 for your age!")
+  alert("Make sure you use full/whole numbers between 1-120 for your age!");
 }
