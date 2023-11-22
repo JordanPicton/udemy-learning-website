d54d3a4773edb1711cb0a124c8f292fbf57873e7
Jordan Picton <jordan_picton@hotmail.com>
2023-11-22 22:43:02 +0000

    Completed Section 17 Chapter 178 Array intro

---
+++ javascript/arrays/scripts/array-intro.js
@@ -0,0 +1,20 @@
+// Creating Arrays
+// The below array is an empty Array and things can be added to it later.
+let emptyArray = [];
+console.log(emptyArray);
+
+// The below array is full of Strings with pre-populated data.
+let stringArray = ['dog', 'cat', 'fish', 'horse'];
+console.log(stringArray);
+
+// The below array is full of Numbers with pre-populated data.
+let numbersArray = [22, 82, 42, 72, 56];
+console.log(numbersArray);
+
+// And below is a mixed array which has a boolean value, number, String, and a falsy value.
+let mixedArray = [false, 68, 'dog', null];
+console.log(mixedArray);
+
+// The other use of the square brackets I've seen so far.
+const testName = "Jordan";
+console.log(testName[3]); // Will print out "d".
