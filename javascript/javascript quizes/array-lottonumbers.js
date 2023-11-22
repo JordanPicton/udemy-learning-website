eb660540645558ebfb276c218b4ad6175bda9cd4
Jordan Picton <jordan_picton@hotmail.com>
2023-11-22 22:45:20 +0000

    Completed Exercise 25 LottoNumber Array

---
+++ javascript/javascript quizes/array-lottonumbers.js
@@ -0,0 +1,3 @@
+// Let's get some practice creating arrays. Please define a variable called lottoNumbers and set it equal to an array containing any 6 numbers.  It's simple, but we've got to start somewhere!
+// Define lottoNumbers below:
+let lottoNumbers = [23, 65, 32, 75, 27, 93];
