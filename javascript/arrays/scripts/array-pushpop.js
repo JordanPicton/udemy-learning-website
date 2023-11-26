a5d87523ea313c9873d83bc94a906d63462ea4e2
Jordan Picton <jordan_picton@hotmail.com>
2023-11-26 21:25:38 +0000

    Learning Array Methods

---
+++ javascript/arrays/scripts/array-pushpop.js
@@ -0,0 +1,10 @@
+let favGames = ["Minecraft", "Terraria", "Factorio"];
+let favAnime = ["Goblin Slayer", "Fire Force", "Sword Art Online"];
+
+// Push Method Example
+favGames.push("Call of Duty"); //favGames will now have the value of "Minecraft", "Terraria", "Factorio", "Call of Duty".
+
+favAnime.push("Naruto");
+
+//Pop Method Example
+console.log(favAnime);
