2cecfa53782679933e771f004e5a19b0602255de
Jordan Picton <jordan_picton@hotmail.com>
2023-11-27 12:38:53 +0000

    Completed Section 17 Chapter 180

--- javascript/arrays/scripts/array-pushpop.js
+++ javascript/arrays/scripts/array-pushpop.js
@@ -3,8 +3,25 @@ let favAnime = ["Goblin Slayer", "Fire Force", "Sword Art Online"];
 
 // Push Method Example
 favGames.push("Call of Duty"); //favGames will now have the value of "Minecraft", "Terraria", "Factorio", "Call of Duty".
+console.log(favGames);
 
 favAnime.push("Naruto");
 
+// Multiple data with Push
+favGames.push("DayZ", "Ark Survival");
+console.log(favGames); // favGames should now have the array length of 6 and the value of "Minecraft", "Terraria", "Factorio", "Call of Duty", "DayZ", "Ark Survival" //It does.
+
 //Pop Method Example
 console.log(favAnime);
+favAnime.pop();
+console.log(favAnime);
+
+// Barbell Example to show why using them in conjunction can be used.
+let barbell = [];
+barbell.push(45);
+barbell.push(45);
+barbell.push(25);
+barbell.push(10);
+barbell.push(2.5);
+
+console.log(barbell);
