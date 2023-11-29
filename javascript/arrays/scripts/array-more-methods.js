66972f338a3cf2066fd0016711fed460360546db
Jordan Picton <jordan_picton@hotmail.com>
2023-11-29 23:23:08 +0000

    Learning Array Methods

---
+++ javascript/arrays/scripts/array-more-methods.js
@@ -0,0 +1,7 @@
+let games = ["Minecraft", "Terraria", "DayZ", "Call of Duty", "Factorio"];
+let animals = ["Dog", "Cat", "Horse", "Pig", "Chicken", "Duck", "Snake"];
+let cars = ["Toyota", "Nissan", "Vauxhall", "BMW", "Tesla", "Ford"];
+
+// The first test is to see if I am correct in how I think concat works with arrays.
+let concatArray = games.concat(cars);
+console.log(concatArray); // This seems to do what I thought it would do.
