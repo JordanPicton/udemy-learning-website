e03f666bd4cc797ce2bd489dfb73f242aa75e469
Jordan Picton <jordan_picton@hotmail.com>
2023-12-18 23:21:34 +0000

    Started Section 17 Chapter 186

---
+++ javascript/arrays/scripts/array-nested.js
@@ -0,0 +1,6 @@
+// An example of a nested array would look like:
+let games = [["Doom Eternal", "Apex Legends", "Titanfall 2", "Destiny 2", "Half-Life 2", "Superhot"], ["Minecraft", "Terraria", "Vintage Story"], ["Ark Survival Evolved", "Rust", "DayZ", "Subnautic"]];
+console.log(games);
+
+// Another example would be something like:
+let notesncrosses = [["O", null, "X"], [null, "X", "O"], ["X", "O", null]];
