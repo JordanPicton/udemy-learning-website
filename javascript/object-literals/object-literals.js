0f0efc70c15e182db4aa4152632a60f5b5848d24
Jordan Picton <jordan_picton@hotmail.com>
2024-03-09 18:15:15 +0000

    Completed Section 18 Chapter 190

--- javascript/object-literals/object-literals.js
+++ javascript/object-literals/object-literals.js
@@ -1,2 +1,11 @@
 // First Object Literal Creation, the current player object has 2 key value pairs.
-const player = {playerName: "Jordan", lootedItems: 204}
+const playerOne = {playerName: "Jordan", lootedItems: 204}
+
+// Another Object Literal, showing off another version with more types.
+const playerTwo = {
+    playerName: "Jordan",
+    blocksMined: 321321,
+    itemsCrafted: 232,
+    monstersSlain: 327457547431,
+    tags: ["isAdmin", "hasPermission"]
+}
