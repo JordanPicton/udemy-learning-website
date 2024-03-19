991aca7fe2fb11e6fc9cdc79fc5961306ea429d8
Jordan Picton <jordan_picton@hotmail.com>
2024-03-19 21:23:32 +0000

    Completed Coding Exercise 30, though I needed help with solution.

---
+++ javascript/javascript quizes/objectliteral-exercise.js
@@ -0,0 +1,36 @@
+// Object Access Exercise
+// In index.js, you'll find a restaurant object that contains a name, address, city, state, and zipcode.
+
+// Your task to is to create a variable named fullAddress that points to a string using the information from restaurant.
+
+// fullAddress should point to a string that includes the address, city, state, and zipcode from the restaurant object. Make sure to add any necessary commas or spaces between the values as seen in the exact expected output format shown below.
+
+// To make it harder to "cheat" by copy and pasting, I've randomly generated the address portion.    For example, if address was "64 Johnson Ave", the fullAddress variable would look like this:
+
+// "64 Johnson Ave, Brooklyn, NY 11206" // this is the expected output format of the fullAddress string (keep an eye on the commas and/or spaces between the values and match that too!)
+
+//PLEASE DON'T TOUCH THIS LINE!
+const restaurant = {
+  name: "Ichiran Ramen",
+  address: `${Math.floor(Math.random() * 100) + 1} Johnson Ave`,
+  city: "Brooklyn",
+  state: "NY",
+  zipcode: "11206"
+};
+
+//YOUR CODE GOES DOWN HERE:
+let fullAddress = "address" + "city" + "state" + "zipcode";
+
+// So below are the different solutions given to figure this out and honestly it makes sense but I'm not sure why I couldn't think of anything.
+
+// SOLUTION #1:
+const fullAddress = `${restaurant.address}, ${restaurant.city}, ${restaurant.state} ${restaurant.zipcode}`;
+
+// SOLUTION #2:
+const fullAddress = `${restaurant["address"]}, ${restaurant["city"]}, ${restaurant["state"]} ${restaurant["zipcode"]}`;
+
+// SOLUTION #3:
+const fullAddress = restaurant.address + ", " + restaurant.city + ", " + restaurant.state + " " + restaurant.zipcode;
+
+// SOLUTION #4:
+const fullAddress = restaurant["address"] + ", " + restaurant["city"] + ", " + restaurant["state"] + " " + restaurant["zipcode"];
